4110f564a873f4088c8e945760537a7bec54008a
kidsage <ruhyun92@gmail.com>
2023-03-29 22:02:47 +0900

    스크립트 수정

--- script/script.js
+++ script/script.js
@@ -30,8 +30,4 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-
-    // url open script
-    var page = 'http://minirest.site'
-        
 });
